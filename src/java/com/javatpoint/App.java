486c18c466e6be12e808d898cfadecb500accdc6
karthicks <karthicks@PTLDTP051.POINTELINDIA.COM>
2019-11-14 15:01:14 +0530

    test1

---
+++ src/java/com/javatpoint/App.java
@@ -0,0 +1,13 @@
+package java.com.javatpoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
